8ef7f51c92f640f599945671164e26f81508ceb0
Topias Teittinen <topias_t@hotmail.com>
2020-02-25 19:32:37 +0200

    initial commit

---
+++ src/main/java/com/yourname/fogelmod/Main.java
@@ -0,0 +1,13 @@
+package com.yourname.fogelmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Main.MODID)
+public class Main {
+    public static final String MODID = "fogelmod";
+
+    public static void main(String[] args) {
+
+    }
+
+}
